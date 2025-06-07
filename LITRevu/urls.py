e6d0b22aaf4879f0e278b29c68c8ec310dfdab06
Sébastien Grison <74866413+SebGris@users.noreply.github.com>
2025-06-07 12:17:17 +0200

    Suppression des vues de changement de mot de passe et du validateur de mot de passe. Mise à jour de la page de base pour retirer le lien de changement de mot de passe.

--- LITRevu/urls.py
+++ LITRevu/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.views import (
-    LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView)
+    LoginView, LogoutView)
 from django.urls import path
 import authentication.views
 import review.views
@@ -13,14 +13,6 @@ urlpatterns = [
         redirect_authenticated_user=True),
          name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('change-password/', PasswordChangeView.as_view(
-        template_name='authentication/password_change_form.html'),
-         name='password_change'
-         ),
-    path('change-password-done/', PasswordChangeDoneView.as_view(
-        template_name='authentication/password_change_done.html'),
-         name='password_change_done'
-         ),
     path('signup/', authentication.views.signup_page, name='signup'),
     path('home/', review.views.home, name='home'),
 ]
