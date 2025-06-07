eb24a330a66fd88b78903ffa8c2cc6fbb2b9de2d
Sébastien Grison <74866413+SebGris@users.noreply.github.com>
2025-06-07 14:24:13 +0200

    Ajout des modèles Ticket et UserFollows avec leurs relations et validations associées.

--- review/models.py
+++ review/models.py
@@ -22,6 +22,16 @@ class Review(models.Model):
 
 class UserFollows(models.Model):
     # Your UserFollows model definition goes here
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        related_name='following',
+        on_delete=models.CASCADE
+    )
+    followed_user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        related_name='followed_by',
+        on_delete=models.CASCADE
+    )
 
     class Meta:
         # ensures we don't get multiple UserFollows instances
