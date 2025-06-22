13582e3e07466bed35132b680b5e2d207d4e4009
Sébastien Grison <74866413+SebGris@users.noreply.github.com>
2025-06-22 20:09:19 +0200

    refactor: améliorer les formulaires de ticket et de critique en ajoutant des étiquettes, des styles et en réorganisant le rendu ; création d'un modèle partiel pour le formulaire de critique

--- review/forms.py
+++ review/forms.py
@@ -7,21 +7,34 @@ User = get_user_model()
 
 
 class TicketForm(forms.ModelForm):
-
     class Meta:
         model = models.Ticket
         fields = ['title', 'description', 'image']
+        labels = {
+            'title': 'Titre',
+            'description': 'Description',
+            'image': 'Image',
+        }
+        widgets = {
+            'title': forms.TextInput(attrs={'style': 'border:2px solid #333; width:100%; text-align:center;'}),
+            'description': forms.Textarea(attrs={'style': 'border:2px solid #333; width:100%;', 'rows': 8}),
+        }
 
 
 class ReviewForm(forms.ModelForm):
 
     class Meta:
         model = models.Review
-        fields = ['rating', 'headline', 'body']
+        fields = ['headline', 'rating', 'body']
+        labels = {
+            'headline': 'Titre',
+            'rating': 'Note',
+            'body': 'Commentaire',
+        }
         widgets = {
             'rating': forms.NumberInput(attrs={'class': 'form-control'}),
-            'headline': forms.TextInput(attrs={'class': 'form-control'}),
-            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'headline': forms.TextInput(attrs={'style': 'border:2px solid #333; width:100%;'}),
+            'body': forms.Textarea(attrs={'style': 'border:2px solid #333; width:100%;', 'rows': 8}),
         }
 
 
